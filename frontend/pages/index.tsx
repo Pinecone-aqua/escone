a3b858388d516fd5871108e8a3e3f2b679b5c207
Morning <morning@admins-iMac-3.local>
2023-04-12 12:09:08 +0800

    small bug fixed

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,4 @@
-import tags from "../utils/dummyUsers.json";
 export default function Home() {
-  let tagsID: string = "";
-  tags.map((tag, i) => {
-    tagsID = tagsID + ", " + `"${tag._id}"`;
-  });
-  console.log(tagsID);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
   );
