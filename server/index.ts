ae079b75f863d4bdda593606e4d0820dc4782ee4
uka03 <76432242+uka03@users.noreply.github.com>
2023-04-25 09:54:54 +0800

    Revert "back end nestJS"

---
+++ server/index.ts
@@ -0,0 +1,21 @@
+import cors from "cors";
+import express from "express";
+import dotenv from "dotenv";
+import "./configs/mongooseConfig";
+import User from "./controller/userRouter";
+import recipe from "./controller/recipesRuoter";
+import review from "./controller/reviewRouter";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+app.use(User);
+app.use(recipe);
+app.use(review);
+app.listen(PORT, () => {
+  console.log(`port: http://localhost:${PORT}`);
+});
