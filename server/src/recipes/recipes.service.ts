538b518e95ea81801b52129eee32babda3963789
Yalalt <yalaltgandush@gmail.com>
2023-04-25 12:06:29 +0800

    working on get recipes

---
+++ server/src/recipes/recipes.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Recipe } from './recipe.schema';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class RecipeService {
+  addRecipe: any;
+  constructor(@InjectModel(Recipe.name) private recipeModel: Model<Recipe>) {}
+  recipes = [];
+  async getAllRecipes() {
+    const result = await this.recipeModel.find({});
+    console.log('all recipes: ', result);
+    return result;
+  }
+}
