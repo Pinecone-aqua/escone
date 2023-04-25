6035d524b721a5dac871cc33ecacbef55ad03f69
Morning <morning@admins-iMac-3.local>
2023-04-25 10:08:19 +0800

    backend changed

---
+++ backend/src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { userModule } from './users/users.module';
+
+@Module({
+  imports: [
+    userModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://escone:H4YbL4MhTZWA7WvJ@pineapple-cluster.a1sr54g.mongodb.net/escone?retryWrites=true&w=majority',
+    ),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ backend/src/users/dto/user.create.dto.ts
@@ -0,0 +1,10 @@
+export class CreateUserDto {
+  _id: string;
+  username: string;
+  image: string;
+  email: string;
+  password: string;
+  role: boolean;
+  favorites: string[];
+  created_date: Date;
+}
