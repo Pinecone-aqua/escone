6f75a0864973170fe2e9a06d3a6905acd16a1195
Morning <morning@admins-iMac-1-4.local>
2023-04-26 11:54:04 +0800

    4/26 c

--- admin/pages/index.tsx
+++ admin/pages/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import axios from "axios";
 
 export default function Home() {
-  // axios.get();
+  axios.get("http://localhost:3030/recipe/all").then((res) => {
+    console.log(res.data);
+  });
   return <div>Home</div>;
 }
