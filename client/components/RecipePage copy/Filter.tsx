8f6d6a6fc56c05adce7b9d0302329cde0cbf9529
Morning <morning@admins-iMac-3.local>
2023-04-27 12:50:35 +0800

    filter sectoin tags

--- client/components/RecipePage copy/Filter.tsx
+++ client/components/RecipePage copy/Filter.tsx
@@ -4,20 +4,38 @@ import { TagType } from "../../utils/types";
 
 function Filter() {
   const [tags, setTags] = useState<TagType[] | undefined>();
+  const [isSelect, setIsSelect] = useState<number[]>([]);
   useEffect(() => {
     axios.get("http://localhost:3030/tag/all").then((res) => {
       setTags(res.data);
     });
   }, []);
-  console.log(tags);
+  function selectHandler(index: number) {
+    if (isSelect.some((val) => val == index)) {
+      const i = isSelect.indexOf(index);
+      const newArr = isSelect.splice(i, 1);
+      console.log(newArr);
+      setIsSelect([...isSelect]);
+    } else {
+      setIsSelect([...isSelect, index]);
+    }
+  }
+  console.log();
   return (
     <div>
-      <div className="">
+      <div className="w-full flex flex-wrap gap-3 justify-center">
         {tags &&
-          tags.map((tag, index) => (
-            <div className="" key={index}>
+          tags.map((tag, index: number) => (
+            <button
+              className={`border border-black px-3 py-1 rounded-full ${
+                isSelect.some((val) => val == index) &&
+                `bg-primary text-white border-none`
+              } `}
+              key={index}
+              onClick={() => selectHandler(index)}
+            >
               {tag.name}
-            </div>
+            </button>
           ))}
       </div>
     </div>
