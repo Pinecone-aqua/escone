f856afbb10c68a4c5b13d4f141f8e90fa6dda794
Maral <maralisgoood@gmail.com>
2023-05-03 15:16:30 +0800

    login modal & scss done

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,11 +1,10 @@
+import Layout from "@/components/global/Layout";
+import "@/styles/globals.scss";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
 
-import Layout from '@/components/global/Layout';
-import '@/styles/globals.scss'
-import "primereact/resources/themes/lara-light-indigo/theme.css";     
-import "primereact/resources/primereact.min.css";                                       
-        
-import type { AppProps } from 'next/app'
-
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
