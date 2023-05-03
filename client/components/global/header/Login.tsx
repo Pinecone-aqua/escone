7f9d0b27d036d5b480083c7226e59e11ea59fa74
Maral <maralisgoood@gmail.com>
2023-05-03 15:16:30 +0800

    login modal & scss done

--- client/components/global/header/Login.tsx
+++ client/components/global/header/Login.tsx
@@ -1,6 +1,11 @@
-import axios from "axios";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useState } from "react";
+import { Button } from "primereact/button";
+import { Dialog } from "primereact/dialog";
+import { PrimeIcons } from "primereact/api";
+import { Divider } from "primereact/divider";
+import { InputText } from "primereact/inputtext";
 import Link from "next/link";
-import { useRouter } from "next/router";
 import { Dispatch, SetStateAction } from "react";
 import {
   FaFacebook,
@@ -11,21 +16,22 @@ import {
 } from "react-icons/fa";
 import { IoCloseOutline } from "react-icons/io5";
 
-// destructured variable class
-const linkClass =
-  "flex items-center justify-center w-[64px] h-[64px] rounded-full bg-gray-100";
-const inputClass =
-  "w-[285px] lg:w-[550px] h-[55px] border border-gray-300 rounded-xl placeholder:text-gray-400";
-const linksListClass = "flex justify-center w-1/3 md:w-1/5 mt-3";
-const buttonClass =
-  "w-full h-[55px] border-none bg-primary text-white rounded-xl";
-// ---------------------------
+interface SocialLink {
+  label: string;
+  icon: any;
+  url: string;
+}
 
-type prop = {
-  setShow: Dispatch<SetStateAction<boolean>>;
-};
+const socialLinks: SocialLink[] = [
+  { label: "google", icon: PrimeIcons.GOOGLE, url: "/" },
+  { label: "whatsapp", icon: PrimeIcons.WHATSAPP, url: "/" },
+  { label: "twitter", icon: PrimeIcons.TWITTER, url: "/" },
+  { label: "facebook", icon: PrimeIcons.FACEBOOK, url: "/" },
+  { label: "instagram", icon: PrimeIcons.INSTAGRAM, url: "/" },
+];
 
-const Login = ({ setShow }: prop): JSX.Element => {
+default function Login() {
+  const [visible, setVisible] = useState<boolean>(false);
   const Router = useRouter();
 
   function googleHandler() {
@@ -34,87 +40,47 @@ const Login = ({ setShow }: prop): JSX.Element => {
       .get("http://localhost:3030/user/google")
       .then((res) => Router.push(res.data));
   }
-  const closeLogin = () => {
-    setShow(false);
-  };
+  const modalHeader = (
+    <div className="modal-header">
+      <h1>Login | Register</h1>
+    </div>
+  );
+
   return (
-    <div className="fixed flex items-center justify-center top-0 left-0  bg-black/30 w-screen h-screen z-10">
-      <div className="login-modal w-[335px] h-[575px] bg-white rounded-3xl md:w-[680px] p-3">
-        <div className="login-modal-close flex w-full justify-end">
-          <button
-            className="flex items-center justify-center w-[48px] h-[48px] rounded-full"
-            onClick={closeLogin}
-          >
-            <IoCloseOutline className="fill-gray-600 w-10 h-10 " />
-          </button>
-        </div>
-        <div className="flex flex-col login-modal-content justify-center items-center">
-          <h1 className="text-2xl-semibold text-gray-800">Sign up</h1>
-          <p className="flex flex-col items-center text-gray-600">
-            Sign up with your social media account
-            <span className="block">or email address</span>
-          </p>
-
-          {/* LINKS w ICON */}
-          <ul className="flex flex-wrap items-center m-3 justify-evenly text-gray-800 text-md-reg w-[335px] lg:w-[415px]">
-            <li className={linksListClass} onClick={googleHandler}>
-              <Link href={"#"} className={linkClass}>
-                <FaGoogle className="w-[32px] h-[32px]" />
-              </Link>
-            </li>
-            <li className={linksListClass}>
-              <Link href={"#"} className={linkClass}>
-                <FaPinterest className="w-[32px] h-[32px]" />
-              </Link>
-            </li>
-            <li className={linksListClass}>
-              <Link href={"#"} className={linkClass}>
-                <FaTwitter className="w-[32px] h-[32px]" />
-              </Link>
-            </li>
-            <li className={linksListClass}>
-              <Link href={"#"} className={linkClass}>
-                <FaInstagram className="w-[32px] h-[32px]" />
-              </Link>
-            </li>
-            <li className={linksListClass}>
-              <Link href={"#"} className={linkClass}>
-                <FaFacebook className="w-[32px] h-[32px]" />
-              </Link>
-            </li>
-          </ul>
-
-          {/* DIVIDER */}
-          <div className="relative flex py-3 items-center">
-            <div className="flex-grow border-t border-gray-400" />
-            <span className="flex-shrink mx-4 text-gray-400">OR</span>
-            <div className="flex-grow border-t border-gray-400" />
+    <div className="login-button mx-5">
+      <Button
+        label="Login"
+        onClick={() => setVisible(true)}
+        className="bg-primary border-none"
+      />
+      <Dialog
+        header={modalHeader}
+        visible={visible}
+        className="login"
+        onHide={() => setVisible(false)}
+      >
+        <div className="login-content">
+          <p>Sign up with your social media account or email address</p>
+          <div className="social-links">
+            <ul>
+              {socialLinks.map((socialLink, index) => (
+                <li key={index} className={socialLink.icon}>
+                  <Link href={socialLink.url} />
+                </li>
+              ))}
+            </ul>
           </div>
-          {/* DIVIDER */}
-
-          <form className="my-2">
-            <label className="mb-2 text-sm font-medium sr-only">Search</label>
-            <div className="relative flex flex-col gap-3">
-              <input
-                type="search"
-                className={inputClass}
-                placeholder="Email Address"
-              />
-              <input
-                type="password"
-                className={inputClass}
-                placeholder="Password"
-              />
-              <div className="buttons flex gap-3">
-                <button className={buttonClass}>Login</button>
-                <button className={buttonClass}>Sign up</button>
-              </div>
-            </div>
-          </form>
+          <Divider align="center">OR</Divider>
+          <div className="login-inputs">
+            <InputText placeholder="Email Address" className="input" />
+            <InputText placeholder="Password" className="input" />
+          </div>
+          <div className="modal-footer">
+            <Button label="Login" onClick={() => setVisible(false)} />
+            <Button label="Register" onClick={() => setVisible(false)} />
+          </div>
         </div>
-      </div>
+      </Dialog>
     </div>
   );
 };
-
-export default Login;
