47de48a31b3a26b0586a4c37b35ea9b5f2b396ed
Yalalt <yalaltgandush@gmail.com>
2023-05-16 13:04:41 +0800

    add recipe not yet finish

--- client/pages/addRecipe.tsx
+++ client/pages/addRecipe.tsx
@@ -190,66 +190,59 @@ export default function AddRecipe({
   }
 
   return (
-    <div className="add container">
-      {!user && <Login show={true} />}
-      <div className="add-head">
-        <h2>Add recipe</h2>
-        <p>
-          post by
-          <span>{user?.username}</span>
-        </p>
-      </div>
-      <form>
-        <label className="flex flex-col gap-3">
-          <p className="text-xl font-semi">Title</p>
-          <input
-            defaultValue={newRecipe.title}
-            type="text"
-            className="border w-full px-3 py-2 rounded-xl"
-            onChange={(e) => {
-              newRecipe.title = e.target.value;
-              setNewRecipe({ ...newRecipe });
-            }}
-          />
-        </label>
-        <div className="flex flex-col gap-3">
-          <p className="text-xl font-semi">Image upload</p>
-          <div className="flex flex-wrap justify-between items-center">
-            {newRecipe.images.map((img: string | undefined, index: number) => (
-              <picture
-                key={index}
-                className="relative group group-hover:delay-300 "
-              >
-                <img src={img} alt="" className="w-40" />
-                <div
-                  className=" absolute top-[-10px] right-[-10px] hidden group-hover:block text-2xl text-gray-400 hover:text-black bg-gray-100 rounded-full"
-                  onClick={() => removeRecipeImage(index)}
-                >
-                  <IoMdCloseCircleOutline />
-                </div>
-              </picture>
-            ))}
-            {images.map((img, index) => (
-              <picture
-                key={index}
-                className="relative group group-hover:delay-300 "
-              >
-                <img src={URL.createObjectURL(img)} alt="" className="w-40" />
-                <div
-                  className=" absolute top-[-10px] right-[-10px] hidden group-hover:block text-2xl text-gray-400 hover:text-black bg-gray-100 rounded-full"
-                  onClick={() => removeImage(index)}
-                >
-                  <IoMdCloseCircleOutline />
-                </div>
-              </picture>
-            ))}
-          </div>
-
-          <input type="file" multiple onChange={uploadHandler} />
+    <div className="add">
+      <div className="container">
+        {!user && <Login show={true} />}
+        <div className="add-head">
+          <h2>Add recipe</h2>
+          <p>
+            post by
+            <span>{user?.username}</span>
+          </p>
         </div>
-        <div className="w-full flex items-center gap-10">
-          <label className="flex items-center gap-3">
-            <p>Servings</p>
+        <form>
+          <div className="title">
+            <label htmlFor="title">title</label>
+            <input
+              defaultValue={newRecipe.title}
+              type="text"
+              onChange={(e) => {
+                newRecipe.title = e.target.value;
+                setNewRecipe({ ...newRecipe });
+              }}
+            />
+          </div>
+          <div className="images">
+            <label htmlFor="image">upload image</label>
+            <div className="images-gallery">
+              {newRecipe.images.map(
+                (img: string | undefined, index: number) => (
+                  <picture key={index}>
+                    <img src={img} alt="" />
+                    <div
+                      className="remove "
+                      onClick={() => removeRecipeImage(index)}
+                    >
+                      <IoMdCloseCircleOutline />
+                    </div>
+                  </picture>
+                )
+              )}
+
+              {images.map((img, index) => (
+                <picture key={index}>
+                  <img src={URL.createObjectURL(img)} alt="" />
+                  <div className="remove " onClick={() => removeImage(index)}>
+                    <IoMdCloseCircleOutline />
+                  </div>
+                </picture>
+              ))}
+            </div>
+
+            <input className="uploader" type="file" onChange={uploadHandler} />
+          </div>
+          <div className="serving">
+            <label htmlFor="serving">serving</label>
             <input
               type="number"
               defaultValue={newRecipe.servings}
@@ -259,9 +252,9 @@ export default function AddRecipe({
                 setNewRecipe({ ...newRecipe });
               }}
             />
-          </label>
-          <label className="flex items-center gap-3">
-            <p>Cook time (min)</p>
+          </div>
+          <div className="cooktime">
+            <label htmlFor="cooktime">cooktime (min)</label>
             <input
               type="number"
               defaultValue={newRecipe.cook_time}
@@ -271,113 +264,110 @@ export default function AddRecipe({
                 setNewRecipe({ ...newRecipe });
               }}
             />
+          </div>
+          <div className="category">
+            <label htmlFor="category">category</label>
+            <div className="flex flex-wrap gap-3">
+              {newRecipe.categories.map((cat: CategoryType) => (
+                <div
+                  className=" relative px-3 py-1 border border-green-500 rounded-full bg-green-100 text-green-800 group "
+                  key={cat._id}
+                >
+                  <p>{cat.name}</p>
+                  <div
+                    className=" absolute top-[-10px] right-[-10px] bg-gray-100 px-2 hover:bg-red-300 text-sm border rounded-full group-hover:block hidden"
+                    onClick={() => removeCategoryHandler(cat._id)}
+                  >
+                    x
+                  </div>
+                </div>
+              ))}
+              <select onChange={addCategoryHandler}>
+                <option defaultChecked>choose category</option>
+                {category.map((cat) => {
+                  if (
+                    !newRecipe.categories.some(
+                      (i: { _id: string }) => i._id == cat._id
+                    )
+                  ) {
+                    return (
+                      <option
+                        value={[cat._id, cat.name, cat.picture]}
+                        key={cat._id}
+                      >
+                        {cat.name}
+                      </option>
+                    );
+                  }
+                })}
+              </select>
+            </div>
+          </div>
+          <div className="tags">
+            <label htmlFor="tags">tags</label>
+            <div className="flex flex-wrap gap-3">
+              <select onChange={addTagHandler}>
+                <option defaultChecked>choose tags</option>
+                {tags.map((tag) => {
+                  if (
+                    !newRecipe.tags.some(
+                      (i: { _id: string }) => i._id == tag._id
+                    )
+                  ) {
+                    return (
+                      <option value={[tag._id, tag.name]} key={tag._id}>
+                        {tag.name}
+                      </option>
+                    );
+                  }
+                })}
+              </select>
+              
+              {newRecipe.tags.map((tag: TagType) => (
+                <div
+                  className="tag relative  group "
+                  key={tag._id}
+                >
+                  <p>{tag.name}</p>
+                  <div
+                    className="remove absolute top-[-10px] right-[-10px] bg-gray-100 px-2 hover:bg-red-300 text-sm border rounded-full group-hover:block hidden"
+                    onClick={() => removeTagHandler(tag._id)}
+                  >
+                    x
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <label className="flex flex-col gap-3">
+            <p className="text-xl font-semi">Description</p>
+            <textarea
+              name=""
+              id=""
+              className="w-full border rounded-lg resize-none p-5 text-lg h-40"
+              defaultValue={newRecipe.description}
+              onChange={(e) => {
+                newRecipe.description = e.target.value;
+                setNewRecipe({ ...newRecipe });
+              }}
+            />
           </label>
-        </div>
-        <label>
-          <p>Categories</p>
-          <div className="flex flex-wrap gap-3">
-            {newRecipe.categories.map((cat: CategoryType) => (
-              <div
-                className=" relative px-3 py-1 border border-green-500 rounded-full bg-green-100 text-green-800 group "
-                key={cat._id}
-              >
-                <p>{cat.name}</p>
-                <div
-                  className=" absolute top-[-10px] right-[-10px] bg-gray-100 px-2 hover:bg-red-300 text-sm border rounded-full group-hover:block hidden"
-                  onClick={() => removeCategoryHandler(cat._id)}
-                >
-                  x
-                </div>
-              </div>
-            ))}
-            <select
-              className="px-3 py-1 border border-blue-200 rounded-full bg-blue-50 text-blue-500 "
-              onChange={addCategoryHandler}
-            >
-              <option defaultChecked>choose category</option>
-              {category.map((cat) => {
-                if (
-                  !newRecipe.categories.some(
-                    (i: { _id: string }) => i._id == cat._id
-                  )
-                ) {
-                  return (
-                    <option
-                      value={[cat._id, cat.name, cat.picture]}
-                      key={cat._id}
-                    >
-                      {cat.name}
-                    </option>
-                  );
-                }
-              })}
-            </select>
-          </div>
-        </label>
-        <label>
-          <p>Tags</p>
-          <div className="flex flex-wrap gap-3">
-            {newRecipe.tags.map((tag: TagType) => (
-              <div
-                className=" relative px-3 py-1 border border-orange-500 rounded-full bg-orange-100 text-orange-800 group "
-                key={tag._id}
-              >
-                <p>{tag.name}</p>
-                <div
-                  className=" absolute top-[-10px] right-[-10px] bg-gray-100 px-2 hover:bg-red-300 text-sm border rounded-full group-hover:block hidden"
-                  onClick={() => removeTagHandler(tag._id)}
-                >
-                  x
-                </div>
-              </div>
-            ))}
-            <select
-              className="px-3 py-1 border border-blue-200 rounded-full bg-blue-50 text-blue-500 "
-              onChange={addTagHandler}
-            >
-              <option defaultChecked>choose category</option>
-              {tags.map((tag) => {
-                if (
-                  !newRecipe.tags.some((i: { _id: string }) => i._id == tag._id)
-                ) {
-                  return (
-                    <option value={[tag._id, tag.name]} key={tag._id}>
-                      {tag.name}
-                    </option>
-                  );
-                }
-              })}
-            </select>
-          </div>
-        </label>
-        <label className="flex flex-col gap-3">
-          <p className="text-xl font-semi">Description</p>
-          <textarea
-            name=""
-            id=""
-            className="w-full border rounded-lg resize-none p-5 text-lg h-40"
-            defaultValue={newRecipe.description}
-            onChange={(e) => {
-              newRecipe.description = e.target.value;
-              setNewRecipe({ ...newRecipe });
-            }}
+          <Ingredient newRecipe={newRecipe} setNewRecipe={setNewRecipe} />
+          <Instructions newRecipe={newRecipe} setNewRecipe={setNewRecipe} />
+          .
+          <input
+            type="button"
+            value={`${
+              recipe
+                ? `update recipe ${adding ? `O` : ""}`
+                : `add recipe ${adding ? `O` : ""}`
+            }`}
+            disabled={adding}
+            onClick={recipe ? updateRecipe : createRecipe}
+            className=" sticky w-40  bottom-10 px-5 py-3 border border-green-700 bg-green-500 text-green-950 rounded-lg "
           />
-        </label>
-        <Ingredient newRecipe={newRecipe} setNewRecipe={setNewRecipe} />
-        <Instructions newRecipe={newRecipe} setNewRecipe={setNewRecipe} />
-        .
-        <input
-          type="button"
-          value={`${
-            recipe
-              ? `update recipe ${adding ? `O` : ""}`
-              : `add recipe ${adding ? `O` : ""}`
-          }`}
-          disabled={adding}
-          onClick={recipe ? updateRecipe : createRecipe}
-          className=" sticky w-40  bottom-10 px-5 py-3 border border-green-700 bg-green-500 text-green-950 rounded-lg "
-        />
-      </form>
+        </form>
+      </div>
     </div>
   );
 }
