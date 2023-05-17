b5417c30cb3fedb484fac942fffa5c1244455404
Morning <morning@admins-iMac-4.local>
2023-05-17 10:44:00 +0800

    s

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello() {
-    return 'http://localhost:3030';
+    return 'foodie backend';
   }
 }
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as dotenv from 'dotenv';
+dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3030);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
