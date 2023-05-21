0a080cc7427b68fc29cb5c472594c51ecec68cc3
Maral <maralisgoood@gmail.com>
2023-05-21 23:08:07 +0800

    files in pages folder have been improved

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,33 +1,48 @@
 import Layout from "@/components/common/Layout";
-import "react-toastify/dist/ReactToastify.css";
+import ProfileLayout from "@/components/common/ProfileLayout";
+import UserProvider from "@/context/userContext";
+
 import { ToastContainer } from "react-toastify";
-import "@/styles/globals.scss";
+import "react-toastify/dist/ReactToastify.css";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 
+import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
-import UserProvider from "@/context/userContext";
-import ProfileLayout from "@/components/common/ProfileLayout";
-export default function App({ Component, pageProps, router }: AppProps) {
-  if (router.pathname.startsWith("/profile")) {
+import { useRouter } from "next/router";
+import { StrictMode } from "react";
+
+export default function App({ Component, pageProps }: AppProps) {
+  const { pathname } = useRouter();
+
+  if (pathname.startsWith("/profile")) {
     return (
-      <UserProvider>
-        <Layout>
-          <ProfileLayout>
-            <Component {...pageProps} />
-            <ToastContainer />
-          </ProfileLayout>
-        </Layout>
-      </UserProvider>
+      <StrictMode>
+        <UserProvider>
+          <Layout>
+            <ProfileLayout>
+              <div>
+                <Component {...pageProps} />
+                <ToastContainer />
+              </div>
+            </ProfileLayout>
+          </Layout>
+        </UserProvider>
+      </StrictMode>
     );
   }
+
   return (
-    <UserProvider>
-      <Layout>
-        <Component {...pageProps} />
-        <ToastContainer />
-      </Layout>
-    </UserProvider>
+    <StrictMode>
+      <UserProvider>
+        <Layout>
+          <div>
+            <Component {...pageProps} />
+            <ToastContainer />
+          </div>
+        </Layout>
+      </UserProvider>
+    </StrictMode>
   );
 }
