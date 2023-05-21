b2415ed8ba623e477a451967800d4e88ba42139d
Maral <maralisgoood@gmail.com>
2023-05-21 23:29:33 +0800

    recipes page have been improved

--- client/pages/recipes.tsx
+++ client/pages/recipes.tsx
@@ -1,18 +1,23 @@
-import RecipeCard from "@/components/common/RecipeCard";
-import Filter from "@/components/subs/Filter";
-import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { useRouter } from "next/router";
 import { RecipeType } from "@/utils/types";
+import Filter from "@/components/subs/Filter";
 import DropFilter from "@/components/subs/DropFilter";
+import RecipeCard from "@/components/common/RecipeCard";
 
-export default function Recipes({
-  recipes,
-  status,
-}: {
+interface RecipesProps {
   recipes: RecipeType[];
   status: string;
-}) {
+}
+
+type QueryType = {
+  category?: string | string[];
+  ingredient?: string | string[];
+  tag?: string | string[];
+};
+
+export default function Recipes({ recipes, status }: RecipesProps) {
   const { query } = useRouter();
   const [filter, setFilter] = useState<(string[] | undefined)[]>([]);
 
@@ -34,7 +39,6 @@ export default function Recipes({
             : filter.map((res) => res?.join(", ")).join(", ")}
           {` "`}
         </p>
-
         <DropFilter status={status} className="dropfilter" />
       </div>
 
@@ -42,10 +46,9 @@ export default function Recipes({
         <div className="recipes-body-filter">
           <Filter status={status} />
         </div>
-
         <div className="recipes-body-list">
-          {recipes.length == 0 ? (
-            <p>empty</p>
+          {recipes.length === 0 ? (
+            <p>Empty</p>
           ) : (
             recipes.map((recipe, index) => (
               <RecipeCard key={index} recipe={recipe} />
@@ -57,73 +60,55 @@ export default function Recipes({
   );
 }
 
-type queryType = {
-  category?: string | string[];
-  ingredient?: string | string[];
-  tag?: string | string[];
-};
-
-export async function getServerSideProps(context: { query: queryType }) {
-  const queryObj: queryType = context.query;
-
+export async function getServerSideProps({ query }: { query: QueryType }) {
   const queryParams = new URLSearchParams();
   queryParams.append("status", "approve");
+
   const statusResult = await axios.get(
     `${process.env.NEXT_PUBLIC_BACK_END_URL}/recipe/statistics`
   );
   const status = statusResult.data;
 
-  for (const key in queryObj) {
-    const value = queryObj[key as keyof queryType];
+  for (const key in query) {
+    const value = query[key as keyof QueryType];
     if (Array.isArray(value)) {
       value.forEach((v) => {
-        if (key == "category") {
-          const category = status.CategoryStatus.find(
-            (cate: { _id: string; name: string }) => {
-              if (cate.name == v) return cate._id;
-            }
-          );
+        const category = status.CategoryStatus.find(
+          (cate: { _id: string; name: string }) => cate.name === v
+        );
+        const tags = status.tagsStatus.find(
+          (tag: { _id: string; name: string }) => tag.name === v
+        );
+
+        if (key === "category" && category) {
           queryParams.append(key, category._id);
-        } else if (key == "tag") {
-          const tags = status.tagsStatus.find(
-            (tag: { _id: string; name: string }) => {
-              if (tag.name == v) return tag._id;
-            }
-          );
+        } else if (key === "tag" && tags) {
           queryParams.append(key, tags._id);
         } else {
           queryParams.append(key, v);
         }
       });
     } else {
-      if (key == "category") {
-        const category = status.CategoryStatus.find(
-          (cate: { _id: string; name: string }) => {
-            if (cate.name == value) return cate._id;
-          }
-        );
+      const category = status.CategoryStatus.find(
+        (cate: { _id: string; name: string }) => cate.name === value
+      );
+      const tags = status.tagsStatus.find(
+        (tag: { _id: string; name: string }) => tag.name === value
+      );
+      if (key === "category" && category) {
         queryParams.append(key, category._id);
-      } else if (key == "tag") {
-        const tags = status.tagsStatus.find(
-          (tag: { _id: string; name: string }) => {
-            if (tag.name == value) return tag._id;
-          }
-        );
+      } else if (key === "tag" && tags) {
         queryParams.append(key, tags._id);
-      } else {
-        value && queryParams.append(key, value);
+      } else if (value) {
+        queryParams.append(key, value);
       }
     }
   }
 
   const queryString = queryParams.toString();
-  const url = `${process.env.NEXT_PUBLIC_BACK_END_URL}/recipe/recipes?${
-    queryString && `${queryString}`
-  }`;
+  const url = `${process.env.NEXT_PUBLIC_BACK_END_URL}/recipe/recipes?${queryString}`;
   const result = await axios.get(url);
   const recipes = result.data;
-  console.log(queryString);
-  return {
-    props: { recipes, status },
-  };
+
+  return { props: { recipes, status } };
 }
